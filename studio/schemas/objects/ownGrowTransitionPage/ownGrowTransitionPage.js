9531374dfa3a04a618fe82975dc421951e704621
HaloLab Developer <halodev@iMac-HaloLab.local>
2021-07-02 11:43:09 +0300

    add own schema

---
+++ studio/schemas/objects/ownGrowTransitionPage/ownGrowTransitionPage.js
@@ -0,0 +1,45 @@
+export default {
+  name: 'ownGrowTransitionPage',
+  type: 'object',
+  title: 'Page Data',
+  fields: [
+    {
+      name: 'pageRouts',
+      type: 'object',
+      title: 'Routs',
+      fields: [
+        {
+          name: 'routName',
+          type: 'string',
+          title: 'Rout Name'
+        },
+        {
+          name: 'routLink',
+          type: 'reference'
+        }
+      ]
+    },
+    {
+      name: 'pageTitle',
+      type: 'string',
+      title: 'Page Title'
+    },
+    {
+      name: 'pageQuestions',
+      type: 'array',
+      title: 'Page Questions',
+      of: [
+        {
+          name: 'question',
+          type: 'string',
+          title: 'Questions'
+        }
+      ]
+    },
+    {
+      name: 'weCanHelp',
+      type: 'string',
+      title: 'We can help'
+    }
+  ]
+}
