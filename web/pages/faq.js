5f78d5a35fcffd9910ebd116baa88f5f6f7bffc7
HaloLab Developer <halodev@iMac-HaloLab.local>
2021-07-16 15:40:35 +0300

    create faq page

--- web/pages/faq.js
+++ web/pages/faq.js
@@ -1,4 +1,46 @@
-const Faq = () => {
-    return <span>Faq</span>
+import client from '../client'
+import groq from 'groq'
+import PropTypes from 'prop-types'
+import FAQContent from '@/scenes/FAQContent'
+
+const Faq = ({faqContent}) => {
+  return <FAQContent faqContent={faqContent} />
+}
+
+export async function getStaticProps() {
+  const data = await client
+    .fetch(
+      groq`*[_type == "faqPage"][0]{
+        faqContent{
+          title,
+          list[]{
+            title,
+            "list":groupList.list[]{
+              title,
+              description,
+              descriptionList
+            }
+          }
+        }
+      }`
+    )
+    .then((res) => {
+      return {...res}
+    })
+
+  if (!data) {
+    return {
+      notFound: true
+    }
   }
-  export default Faq
+
+  return {
+    props: {...data}
+  }
+}
+
+Faq.propTypes = {
+  faqContent: PropTypes.object
+}
+
+export default Faq
